728a08dd6e1c7341b565f799e1abfaa405a81506
Luis <sssimple.33@gmail.com>
2020-02-03 22:54:27 -0500

    master

--- src/Components/Admin/ManageProject/Search.js
+++ src/Components/Admin/ManageProject/Search.js
@@ -1,6 +1,6 @@
 import React, { useState, useContext, useEffect } from "react";
 import { Context } from "../../../Context";
-
+import {itemsFiltered} from './../../../helpers/FilterItems'
 export function Search({ setFilteredArray }) {
   const { myPersonel } = useContext(Context);
   const [searchedWord, setSearchedWord] = useState("");
@@ -9,30 +9,6 @@ export function Search({ setFilteredArray }) {
     setFilteredArray(myPersonel);
   }, [myPersonel]);
 
-  const itemsFiltered = (searchedItem, arrayOfObject) => {
-    let arrayFiltered = arrayOfObject.filter(function(obj) {
-      let objectClean = {};
-      objectClean["name"] = obj["name"];
-      objectClean["email"] = obj["email"];
-      if (obj["assignedProjects"]) {
-        for (let i = 0; i < obj["assignedProjects"].length; i++) {
-          objectClean[i] = obj["assignedProjects"][i];
-        }
-      } else {
-        objectClean["assignedProjects"] = "";
-      }
-      for (let key in objectClean) {
-        if (
-          JSON.stringify(objectClean[key])
-            .toLowerCase()
-            .includes(searchedItem.toLowerCase())
-        ) {
-          return objectClean;
-        }
-      }
-    });
-    return arrayFiltered;
-  };
 
   const handleSearchedWord = e => {
     setSearchedWord(e.target.value);
